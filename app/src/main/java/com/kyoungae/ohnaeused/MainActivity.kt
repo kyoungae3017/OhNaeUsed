d7bf30187e8eb975ebe4631ead6c00f5a6145ff4
kyoungae <kyoungae3017@gmail.com>
2022-01-18 15:19:34 +0900

    first commit

---
+++ app/src/main/java/com/kyoungae/ohnaeused/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.kyoungae.ohnaeused
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
